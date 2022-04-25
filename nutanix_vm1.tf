4263a5d507bcfacdc27a44fca9bfa9dd286107de
Daeric <daericg@gmail.com>
2022-04-25 14:58:58 -0500

    Update nutanix_vm1.tf
    add 3 vms

--- nutanix_vm1.tf
+++ nutanix_vm1.tf
@@ -28,3 +28,41 @@ resource "nutanix_virtual_machine" "VM1" {
     subnet_uuid = data.nutanix_subnet.subnet.id
   }
 }
+
+resource "nutanix_virtual_machine" "VM1" {
+  name                 = "VM2"
+  cluster_uuid         = data.nutanix_cluster.cluster.id
+  num_vcpus_per_socket = "4"
+  num_sockets          = "1"
+  memory_size_mib      = 8192
+
+  disk_list {
+    data_source_reference = {
+      kind = "image"
+      uuid = nutanix_image.CentOS.id
+    }
+  }
+
+  nic_list {
+    subnet_uuid = data.nutanix_subnet.subnet.id
+  }
+}
+
+resource "nutanix_virtual_machine" "VM1" {
+  name                 = "VM3"
+  cluster_uuid         = data.nutanix_cluster.cluster.id
+  num_vcpus_per_socket = "4"
+  num_sockets          = "1"
+  memory_size_mib      = 8192
+
+  disk_list {
+    data_source_reference = {
+      kind = "image"
+      uuid = nutanix_image.CentOS.id
+    }
+  }
+
+  nic_list {
+    subnet_uuid = data.nutanix_subnet.subnet.id
+  }
+}
