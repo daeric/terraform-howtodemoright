f4e88f2774261e4f32889844d245f310f3c375d8
Daeric <daericg@gmail.com>
2022-04-26 09:05:53 -0500

    Update and rename aws_vm1.tf to aws_vm2.tf

--- aws_vm2.tf
+++ aws_vm2.tf
@@ -1,5 +1,6 @@
-resource "aws_instance" "web_instance" {
+resource "aws_instance" vm2" {
   ami           = "ami-0f9fc25dd2506cf6d"
   instance_type = "t2.micro"
   key_name      = "MyKeyPair"
+  subnet_id     = "subnet-01"
 }
