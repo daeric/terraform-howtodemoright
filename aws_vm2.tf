4b8b4a0c3a6fe7c0ca3f8726a44f9c33752f0107
Daeric <daericg@gmail.com>
2022-04-26 11:25:27 -0500

    Update aws_vm2.tf

--- aws_vm2.tf
+++ aws_vm2.tf
@@ -1,6 +1,6 @@
-resource "aws_instance" "vm2" {
-  ami           = "ami-0f9fc25dd2506cf6d"
-  instance_type = "t2.micro"
-  key_name      = "MyKeyPair"
-  subnet_id     = "subnet-0170967c983eec7f0"
-}
+#resource "aws_instance" "vm2" {
+#  ami           = "ami-0f9fc25dd2506cf6d"
+#  instance_type = "t2.micro"
+#  key_name      = "MyKeyPair"
+#  subnet_id     = "subnet-0170967c983eec7f0"
+#}
