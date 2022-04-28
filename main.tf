af151d9df90693acac81221a05e5f905784bf074
Daeric <daericg@gmail.com>
2022-04-28 10:05:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "nutanix_network_security_rule" "isolation" {
     name        = "isolate-prod-from-dev"
     description = "Isolate Production from Dev"
 
-    isolation_rule_action = "APPLY"
+    isolation_rule_action = "MONITOR"
 
     isolation_rule_first_entity_filter_kind_list = ["vm"]
     isolation_rule_first_entity_filter_type      = "CATEGORIES_MATCH_ALL"
