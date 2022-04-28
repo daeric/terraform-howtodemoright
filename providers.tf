bd5f67d5c0d92edb1afabc04667c9f59f6776784
Daeric <daericg@gmail.com>
2022-04-28 03:41:57 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,19 +1,10 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
     nutanix = {
       source  = "nutanix/nutanix"
       version = ">=1.4.1"
     }
   }  
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1"
 }
 
 # Configure the Nutanix Provider
