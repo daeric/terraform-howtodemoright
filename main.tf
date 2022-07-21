29b7dd9d5ab0a8ef370b2e1807f9c9e2247109e5
Daeric <daericg@gmail.com>
2022-07-20 22:55:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
+#Provider info
+terraform {
+  required_providers {
+    nutanix = {
+      source = "nutanix/nutanix"
+      version = ">=1.6.0"
+    }
+  }
+}
 
+provider "nutanix" {
+  username            = var.user
+  password            = var.password
+  endpoint            = var.endpoint
+  port                = 9440
+  insecure            = true
+  wait_timeout        = 10
+  foundation_endpoint = var.foundation_endpoint
+  foundation_port     = var.foundation_port
+}
