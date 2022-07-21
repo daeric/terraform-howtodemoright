baa0e99f6f77f11b9ace287e9d7d3172533b0b67
Daeric <daericg@gmail.com>
2022-07-20 22:57:42 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,9 @@ provider "nutanix" {
   foundation_endpoint = var.foundation_endpoint
   foundation_port     = var.foundation_port
 }
+
+#Import image
+resource "nutanix_image" "Ubuntu" {
+  name        = "Ubuntu"
+  source_uri  = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+}
