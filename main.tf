01e5eecf1550b24631139a5a16ebf7b8c8c2e1f6
Daeric <daericg@gmail.com>
2022-07-20 23:00:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -136,3 +136,25 @@ resource "nutanix_virtual_machine" "DEV" {
     "VSS_SNAPSHOT"
   ]
 }
+
+#Create Isolation Policy
+resource "nutanix_network_security_rule" "isolation" {
+    name        = "isolate-prod-from-dev"
+    description = "Isolate Production from Dev"
+
+    isolation_rule_action = "APPLY"
+
+    isolation_rule_first_entity_filter_kind_list = ["vm"]
+    isolation_rule_first_entity_filter_type      = "CATEGORIES_MATCH_ALL"
+    isolation_rule_first_entity_filter_params {
+        name   = "Environment"
+        values = ["Production"]
+    }
+
+    isolation_rule_second_entity_filter_kind_list = ["vm"]
+    isolation_rule_second_entity_filter_type      = "CATEGORIES_MATCH_ALL"
+    isolation_rule_second_entity_filter_params {
+        name   = "Environment"
+        values = ["Dev"]
+    }
+}
